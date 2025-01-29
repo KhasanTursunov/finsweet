13f1c6d11f315ea3f283b364c2f8f71749bfd14c
KhasanTursunov <khasantursunovrustamovich@gmail.com>
2025-01-29 11:47:00 +0500

    try for vercel

--- src/pages/media/MediaPage.jsx
+++ src/pages/media/MediaPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import HeaderCom from '../../components/header/HeaderCom'
-import MediaShowCom from '../../components/mediaShowcase/mediaShowCom';
+import MediaShowCom from '../../components/mediaShowcase/MediaShowCom';
 import MediaHeroCom from '../../components/mediaHero/MediaHeroCom';
 import OurEvents from '../../components/ourEvents/OurEvents'
 import Footer from '../../components/footer/Footer'
